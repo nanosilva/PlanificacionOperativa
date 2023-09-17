babc51e7f13e982a820f05cc4a59b95684e121a0
nanosilva <seba_nanosilva@hotmail.com>
2023-09-17 15:23:09 -0300

    version final_ok

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,6 @@
 
 export const environment = {
    production: false,
-   firebaseConfig :{
-    apiKey: "AIzaSyAC--Kcyb6NZ5D6fkrdHiUi0OwAWCbCduQ",
-    authDomain: "api-planificacion.firebaseapp.com",
-    databaseURL: "https://api-planificacion-default-rtdb.firebaseio.com",
-    projectId: "api-planificacion",
-    storageBucket: "api-planificacion.appspot.com",
-    messagingSenderId: "828252339481",
-    appId: "1:828252339481:web:1535cfc6d9e1bbf3039988"
-  }
 };
 
 /*
